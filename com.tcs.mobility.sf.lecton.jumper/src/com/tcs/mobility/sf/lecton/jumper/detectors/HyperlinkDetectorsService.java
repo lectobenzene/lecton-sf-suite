c54dd00a39ebcd946d36a87ecc947e956333d028
lectobenzene <lectobenzene@gmail.com>
2014-09-28 21:25:57 +0530

    Jumper poject Initial commit

---
+++ com.tcs.mobility.sf.lecton.jumper/src/com/tcs/mobility/sf/lecton/jumper/detectors/HyperlinkDetectorsService.java
@@ -0,0 +1,20 @@
+package com.tcs.mobility.sf.lecton.jumper.detectors;
+
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+
+public class HyperlinkDetectorsService extends AbstractHyperlinkDetector {
+
+	public HyperlinkDetectorsService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region, boolean canShowMultipleHyperlinks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
