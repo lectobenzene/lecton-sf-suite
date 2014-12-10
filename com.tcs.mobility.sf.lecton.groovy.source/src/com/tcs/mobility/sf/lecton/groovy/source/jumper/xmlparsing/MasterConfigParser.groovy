da49fb6ccb2d54682a04b40a8870dfd2d2a8579d
lectobenzene <lectobenzene@gmail.com>
2014-12-10 22:37:03 +0530

    Groovy project creation

---
+++ com.tcs.mobility.sf.lecton.groovy.source/src/com/tcs/mobility/sf/lecton/groovy/source/jumper/xmlparsing/MasterConfigParser.groovy
@@ -0,0 +1,9 @@
+package com.tcs.mobility.sf.lecton.groovy.source.jumper.xmlparsing
+
+class MasterConfigParser {
+
+	public MasterConfigParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
