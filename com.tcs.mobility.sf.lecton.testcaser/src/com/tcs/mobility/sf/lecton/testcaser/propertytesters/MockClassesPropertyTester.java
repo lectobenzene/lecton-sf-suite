806e24ae09177ada967d6692ea4e0ea6cbb1aedc
lectobenzene <lectobenzene@gmail.com>
2015-01-03 14:16:20 +0530

    Menu contributions refactored, testcaser plugin entry point created

---
+++ com.tcs.mobility.sf.lecton.testcaser/src/com/tcs/mobility/sf/lecton/testcaser/propertytesters/MockClassesPropertyTester.java
@@ -0,0 +1,32 @@
+package com.tcs.mobility.sf.lecton.testcaser.propertytesters;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
+
+import com.tcs.mobility.sf.lecton.utility.utils.UtilResource;
+
+public class MockClassesPropertyTester extends PropertyTester {
+
+	public static final String PROPERTY_FILE_TYPE = "filetype";
+	
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		System.out.println("Property Testing");
+		if (PROPERTY_FILE_TYPE.equals(property)) {
+			if(receiver instanceof ICompilationUnit){
+				// Make it visible only if the expected extension is satisfied. The
+				// extension should be 'java'
+				if (((String) expectedValue).equals(UtilResource.getFileExtension(((ICompilationUnit)receiver).getElementName()))) {
+					return true;
+				}
+			}else if(receiver instanceof IPackageFragment){
+				// show command for an entire package
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
--- com.tcs.mobility.sf.lecton.utility/src/com/tcs/mobility/sf/lecton/utility/utils/UtilResource.java
+++ com.tcs.mobility.sf.lecton.utility/src/com/tcs/mobility/sf/lecton/utility/utils/UtilResource.java
@@ -84,4 +84,28 @@ public class UtilResource {
 			return ResourcesPlugin.getWorkspace().getRoot().getFolder(fullPath);
 		}
 	}
+	
+	/**
+	 * Returns the file name without the extension part
+	 * 
+	 * @param fileName
+	 *            Name of the file
+	 * @return
+	 */
+	public static String getFileNameWithoutExtension(String fileName) {
+		int lastIndex = fileName.lastIndexOf(".");
+		return fileName.substring(0, lastIndex);
+	}
+
+	/**
+	 * Returns the extension of the file
+	 * 
+	 * @param fileName
+	 *            name of the file
+	 * @return
+	 */
+	public static String getFileExtension(String fileName) {
+		String[] split = fileName.split("\\.");
+		return split[split.length - 1];
+	}
 }
